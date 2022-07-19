810a4bc5fd3cde1e84ea26c7abfdacde1ca24d94
卢照旭 <luzx23@chinaunicom.cn>
2022-07-19 15:53:51 +0800

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// Todo: add pprof tools server
+
+	// parse flage of all flags for Captain
+
+	// Todo: start CapServer
+
+	// Todo: process signal of os
+	return
+}
---
+++ pkg/router/kuberesource/resources.go
@@ -0,0 +1,5 @@
+package kuberesource
+
+// func ()  {
+
+// }
