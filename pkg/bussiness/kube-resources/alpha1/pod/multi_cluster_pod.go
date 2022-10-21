27d82f26ffbb80f32d32e2581ee051078bb021ff
zhouzf37 <zhouzf37@chinaunicom.cn>
2022-10-21 10:49:45 +0800

    修改pod

--- pkg/bussiness/kube-resources/alpha1/pod/multi_cluster_pod.go
+++ pkg/bussiness/kube-resources/alpha1/pod/multi_cluster_pod.go
@@ -2,8 +2,6 @@ package pod
 
 import (
 	"context"
-	"strings"
-
 	appv1 "k8s.io/api/apps/v1"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -66,82 +64,21 @@ func (c *PodProviderClient) filter(object runtime.Object, filter query.Filter) b
 	if !ok {
 		return false
 	}
-
+	if !ok {
+		return false
+	}
 	switch filter.Field {
-	case query.FieldOwnerKind:
-		fallthrough
-	case query.FieldOwnerName:
-		kind := filter.Field
-		name := filter.Value
-		if !c.podBelongTo(pod, string(kind), string(name)) {
-			return false
-		}
-	case "nodeName":
-		if pod.Spec.NodeName != string(filter.Value) {
-			return false
-		}
-	case "pvcName":
-		if !podBindPVC(pod, string(filter.Value)) {
-			return false
-		}
-	case "serviceName":
-		if !c.podBelongToService(pod, string(filter.Value)) {
-			return false
-		}
-	case query.FieldStatus:
-		return strings.Compare(string(pod.Status.Phase), string(filter.Value)) == 0
+	case fieldNodeName:
+		return pod.Spec.NodeName == string(filter.Value)
+	case fieldPVCName:
+		return podBindPVC(pod, string(filter.Value))
+	case fieldServiceName:
+		return c.podBelongToService(pod, string(filter.Value))
+	case fieldStatus:
+		return string(pod.Status.Phase) == string(filter.Value)
 	default:
 		return alpha1.DefaultObjectMetaFilter(pod.ObjectMeta, filter)
 	}
-	return false
-}
-
-func (c *PodProviderClient) podBelongTo(item *v1.Pod, kind string, name string) bool {
-	switch kind {
-	case "Deployment":
-		if c.podBelongToDeployment(item, name) {
-			return true
-		}
-	case "ReplicaSet":
-		if podBelongToReplicaSet(item, name) {
-			return true
-		}
-	case "DaemonSet":
-		if podBelongToDaemonSet(item, name) {
-			return true
-		}
-	case "StatefulSet":
-		if podBelongToStatefulSet(item, name) {
-			return true
-		}
-	case "Job":
-		if podBelongToJob(item, name) {
-			return true
-		}
-	}
-	return false
-}
-
-func (c *PodProviderClient) podBelongToDeployment(item *v1.Pod, deploymentName string) bool {
-	var list *appv1.ReplicaSetList
-	if c.replicaSets != nil {
-		list = c.replicaSets
-	} else {
-		list, err := c.AppsV1().ReplicaSets(item.Namespace).List(context.Background(), metav1.ListOptions{})
-		if err != nil {
-			return false
-		}
-		c.replicaSets = list
-	}
-	if list != nil && list.Items != nil {
-		for i := 0; i < len(list.Items); i++ {
-			r := &list.Items[i]
-			if replicaSetBelongToDeployment(r, deploymentName) && podBelongToReplicaSet(item, r.Name) {
-				return true
-			}
-		}
-	}
-	return false
 }
 
 func (c *PodProviderClient) podBelongToService(item *v1.Pod, serviceName string) bool {
